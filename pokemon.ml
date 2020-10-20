6f5ee876eec2317837922426e87aede446494fda
Aira <aa2235@cornell.edu>
2020-10-20 13:36:17 -0400

    implement pokemon.ml and pokemon.mli

--- pokemon.ml
+++ pokemon.ml
@@ -1,1 +1,103 @@
-type t = string
+open Yojson.Basic.Util
+
+type name = string
+type poke_type = 
+  | Bug | Dark | Dragon | Electric | Fighting | Fire | Flying | Ghost 
+  | Grass | Ice | Normal | Poison | Psychic | Rock | Steel | Water
+type level = int
+type hp = int
+type attack = int
+type defense = int
+type curr_exp = int
+type level_up_exp = int
+type caught = bool
+exception InvalidPokemon of string
+
+type move = {
+  move_type: poke_type;
+  move_name: string;
+}
+
+type stats = {
+  level: level;
+  hp: hp;
+  attack: attack;
+  defense: defense;
+  curr_exp: curr_exp;
+  level_up_exp: level_up_exp;
+}
+
+type t = {
+  name: name;
+  poke_type: poke_type;
+  stats: stats;
+  caught: caught;
+  move_set: move list
+}
+
+let poke_from_json pokemon = 
+  failwith "Unimplemented"
+
+let get_name pokemon = pokemon.name
+
+let get_poke_type pokemon = pokemon.poke_type 
+
+let get_level pokemon = pokemon.stats.level
+
+let get_hp pokemon = pokemon.stats.hp
+
+let get_attack pokemon = pokemon.stats.attack
+
+let get_defense pokemon = pokemon.stats.defense
+
+let get_curr_exp pokemon = pokemon.stats.curr_exp
+
+let get_level_up_exp pokemon = pokemon.stats.level_up_exp
+
+let get_caught pokemon = pokemon.caught
+
+let get_move pokemon move_name = 
+  match List.filter (fun move' -> move'.move_name = move_name) 
+          pokemon.move_set with
+  | [] -> raise (InvalidPokemon "pokemon is not valid")
+  | h :: t -> h
+
+let level_up pokemon = 
+  let curr_stats = pokemon.stats in
+  if curr_stats.curr_exp > curr_stats.level_up_exp then 
+    let new_stats = {
+      level = curr_stats.level + 1;
+      hp = curr_stats.hp + 2;
+      attack = curr_stats.attack + 2;
+      defense = curr_stats.defense + 2;
+      curr_exp = curr_stats.level_up_exp - curr_stats.curr_exp;
+      level_up_exp = curr_stats.level_up_exp + 2;
+    } in 
+    {
+      name = pokemon.name;
+      poke_type = pokemon.poke_type;
+      stats = new_stats;
+      caught = pokemon.caught;
+      move_set = pokemon.move_set;
+    }
+  else pokemon
+
+let increase_exp p1 p2 = 
+  let p2_lvl = float_of_int p2.stats.level in 
+  let exp = int_of_float (p2_lvl *. 0.5) in 
+  let curr_stats = p1.stats in 
+  let new_stats = {
+    level = curr_stats.level;
+    hp = curr_stats.hp;
+    attack = curr_stats.attack;
+    defense = curr_stats.defense;
+    curr_exp = curr_stats.level_up_exp + exp;
+    level_up_exp = curr_stats.level_up_exp;
+  } in 
+  { 
+    name = p1.name;
+    poke_type = p1.poke_type;
+    stats = new_stats;
+    caught = p1.caught;
+    move_set = p1.move_set;
+  }
--- pokemon.mli
+++ pokemon.mli
@@ -1,2 +1,86 @@
+(** 
+   Representation of pokemon data.
+
+   This module represents the data of a pokemon file, including their name, 
+   type, level, HP, attack, defense, experience, move set, and caught status. 
+   It also handles loading of a pokemon's data from JSON.
+*)
+
 (** The abstract type of values representing a pokemon. *)
 type t
+
+(** The name of a pokemon *)
+type name = string
+
+(** The type of a pokemon's type *)
+type poke_type
+
+(** The type of a pokemon's level *)
+type level = int
+
+(** The type of a pokemon's HP stat *)
+type hp = int
+
+(** The type of a pokemon's attack stat *)
+type attack = int
+
+(** The type of a pokemon's defense stat *)
+type defense = int
+
+(** The type of a pokemon's current experience *)
+type curr_exp = int
+
+(** The type of the experience needed for the pokemon to level up *)
+type level_up_exp = int
+
+(** The type of a pokemon's current caught status *) 
+type caught = bool
+
+(** The type of a pokemon's move *)
+type move 
+
+(** Raised when an invalid pokemon is encountered. *)
+exception InvalidPokemon of string
+
+(** [poke_from_json j] is the pokemon that [j] represents.
+    Requires: [j] is a valid JSON pokemon representation. *)
+val poke_from_json : Yojson.Basic.t -> t
+
+(** [get_name t] is the name of pokemon [t]. *)
+val get_name : t -> name
+
+(** [get_poke_type t] is the poke_type of pokemon [t]. *)
+val get_poke_type : t -> poke_type
+
+(** [get_poke_type t] is the level of pokemon [t]. *)
+val get_level : t -> level
+
+(** [get_hp t] is the hp of pokemon [t]. *)
+val get_hp : t -> hp
+
+(** [get_attack t] is the attack of pokemon [t]. *)
+val get_attack : t -> attack
+
+(** [get_defense t] is the defense of pokemon [t]. *)
+val get_defense : t -> defense
+
+(** [get_curr_exp t is the current experience of pokemon [t]. *)
+val get_curr_exp: t -> curr_exp
+
+(** [get_level_up_exp t] is the level_up_exp of pokemon [t]. *)
+val get_level_up_exp : t -> level_up_exp
+
+(** [get_caught t] is the caught status of pokemon [t]. *)
+val get_caught : t -> caught
+
+(** [get_move t move_name] is the [move] represented by [move_name]. *)
+val get_move : t -> string -> move
+
+(** [level_up t] is pokemon [t] with [t]'s level incremented by one if 
+    [t]'s curr_exp exceeds [t]'s level_up_exp and [t]'s hp, attack, and defense
+    are incremented accordinglt. *)
+val level_up : t -> t
+
+(** [increase_exp p1 p2] is pokemon [p1]'s curr_exp incremented based on
+    the stats of pokemon [p2]. *)
+val increase_exp : t -> t -> t
