369c7ad8c069c81234fcc765709b0f6b00fe04c6
Amy Huang <ach243@cornell.edu>
2020-12-20 14:39:15 -0600

    haha pt3

--- walking.mli
+++ walking.mli
@@ -5,6 +5,9 @@ val parse_bag : Player.player -> string
     panel *)
 val parse_pokelist : Player.player -> string
 
+(** TODO - add doc *)
+val trainer_on_block : State.state -> Trainer.trainer
+
 (** [process_walk input st] is the state after some key input while the 
     player is walking *)
 val process_walk : char -> State.state -> State.state
